682afd7db7f6acfeadaaa2ea1cfa5a529601eea9
Walker Forrester <39706348+walkforr@users.noreply.github.com>
2018-12-27 17:43:35 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -13,21 +13,6 @@ window.onclick = function(event) {
     }
 }
 
-// var modal2 = document.getElementById('myModal2');
-// var signin = document.getElementById("signIn");
-// var span2 = document.getElementsByClassName("close2")[0];
-// signin.onclick = function() {
-//     modal2.style.display = "block";
-// }
-// span2.onclick = function() {
-//     modal2.style.display = "none";
-// }
-// window.onclick = function(event) {
-//     if (event.target == modal2) {
-//         modal2.style.display = "none";
-//     }
-// }
-
 
 var modal3 = document.getElementById('myModal3');
 var signup = document.getElementById("signUp");
